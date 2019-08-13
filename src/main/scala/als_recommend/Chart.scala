d3544345f62e84e02a2a7c58fa2aa6a6c0398b6b
erpeng.qi <erpeng.qi@ly.com>
2019-08-13 11:34:41 +0800

    add

---
+++ src/main/scala/als_recommend/Chart.scala
@@ -0,0 +1,57 @@
+package als_recommend
+
+import java.awt.Color
+
+import org.jfree.chart.{ChartFactory, ChartFrame}
+import org.jfree.chart.axis.{AxisLocation, NumberAxis}
+import org.jfree.chart.labels.StandardCategoryToolTipGenerator
+import org.jfree.chart.plot.DatasetRenderingOrder
+import org.jfree.chart.renderer.category.LineAndShapeRenderer
+import org.jfree.data.category.DefaultCategoryDataset
+
+object Chart {
+
+  def plotBarLineChart(title: String, xLable: String, yBarLabel: String, yBarMin: Double, yBarMax: Double, yLineLabel: String, dataBarChart: DefaultCategoryDataset, dataLinechart: DefaultCategoryDataset): Unit = {
+
+    // draw bar chart
+    val chart = ChartFactory
+      .createBarChart(
+        "",
+        xLable,
+        yBarLabel,
+        dataBarChart,
+        org.jfree.chart.plot.PlotOrientation.VERTICAL,
+        true, // include legend
+        true, // display tooltips
+        false // remove url generator
+      )
+
+    val plot = chart.getCategoryPlot
+    plot.setBackgroundPaint(new Color(255, 255, 255))
+    plot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_RIGHT)
+    plot.setDataset(, dataLinechart)
+    plot.mapDatasetToRangeAxis(1, 1)
+
+    // draw bar y
+    val vn = plot.getRangeAxis
+    vn.setRange(yBarMin, yBarMax)
+    vn.setAutoTickUnitSelection(true)
+
+    // draw line y
+    val axis2 = new NumberAxis(yLineLabel)
+    plot.setRangeAxis(1, axis2)
+    val renderer2 = new LineAndShapeRenderer()
+    renderer2.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator())
+
+    // draw bar first
+    plot.setRenderer(1, renderer2)
+    plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD)
+
+    val frame = new ChartFrame(title, chart)
+    frame.setSize(10, 10)
+    frame.pack()
+    frame.setVisible(true)
+
+  }
+
+}
--- src/main/scala/als_recommend/GridSearch.scala
+++ src/main/scala/als_recommend/GridSearch.scala
@@ -1,6 +1,78 @@
 package als_recommend
 
+import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+import org.apache.spark.rdd.RDD
+import org.jfree.data.category.DefaultCategoryDataset
+import org.joda.time.{DateTime, Duration}
+
 object GridSearch {
 
+  def main(args: Array[String]): Unit = {
+    val (ratingRDD, movieTitle) = Recommend.PrepareData()
+    val Array(trainData: RDD[Rating], validationData: RDD[Rating], testData: RDD[Rating]) = ratingRDD.randomSplit(Array(0.8, 0.1, 0.1))
+
+  }
+
+  def trainValidation(trainData: RDD[Rating], validationData: RDD[Rating]): MatrixFactorizationModel = {
+    println("========== EVALUATE PARAM: RANK ==========")
+    evaluateParameter(trainData, validationData, "rank", Array(5, 10, 15, 20, 50, 100), Array(10), Array(0.1))
+
+    println("========== EVALUATE PARAM: NUMBER OF ITERATIONS ==========")
+    evaluateParameter(trainData, validationData, "numIterations", Array(10), Array(5, 10, 15, 20, 25), Array(0.1))
+
+    println("========== EVALUATE PARAM: LAMBDA ==========")
+    evaluateParameter(trainData, validationData, "rank", Array(10), Array(10), Array(0.05, 0.1, 1, 5, 10))
+
+    println("========== EVALUATE PARAM: PARAM COMBINATIONS ==========")
+    evaluateAllParameter(trainData, validationData, "rank", Array(5, 10, 15, 20, 50, 100), Array(5, 10, 15, 20, 25), Array(0.05, 0.1, 1, 5, 10))
+  }
+
+  def evaluateParameter(trainData: RDD[Rating], validationData: RDD[Rating], evaluateParameter: String, rankArray: Array[Int], numIterationsArray: Array[Int], lambdaArray: Array[Double]) = {
+    var dataBarChart = new DefaultCategoryDataset()
+    var dataLineChart = new DefaultCategoryDataset()
+    for (rank <- rankArray; numIterations <- numIterationsArray; lambda <- lambdaArray) {
+      val (rmse, time) = trainModel(trainData, validationData, rank, numIterations, lambda)
+      val parameterData =
+        evaluateParameter match {
+          case "rank" => rank;
+          case "numIterations" => numIterations;
+          case "lambda" => lambda
+        }
+      dataBarChart.addValue(rmse, evaluateParameter, parameterData.toString)
+      dataLineChart.addValue(time, "TIME", parameterData.toString)
+      Chart.plotBarLineChart("ALS_EVALUATIONS " + evaluateParameter, evaluateParameter, "RMSE", 0.58, 5, "TIME", dataBarChart, dataLineChart)
+    }
+  }
+
+  def trainModel(trainData: RDD[Rating], validationData: RDD[Rating], rank: Int, numIterations: Int, lambda: Double): (Double, Double) = {
+    val startTime = new DateTime()
+    val model = ALS.train(trainData, rank, numIterations, lambda)
+    val endTime = new DateTime()
+    val rmse = computeRmse(model, validationData)
+    val duration = new Duration(startTime, endTime)
+    (rmse, duration.getStandardSeconds)
+  }
+
+  def computeRmse(model: MatrixFactorizationModel, ratingRDD: RDD[Rating]): Double = {
+    val num = ratingRDD.count()
+    val predictedRDD = model.predict(ratingRDD.map(r => (r.user, r.product)))
+    val predictedAndRatings =
+      predictedRDD.map(p => ((p.user, p.product), p.rating))
+        .join(ratingRDD.map(r => ((r.user, r.product), r.rating)))
+        .values
+    math.sqrt(predictedAndRatings.map(x => math.pow(x._1 - x._2, 2)).reduce(_ + _) / num)
+  }
+
+  def evaluateAllParameter(trainData: RDD[Rating], validationData: RDD[Rating], evaluateParameter: String, rankArray: Array[Int], numIterationsArray: Array[Int], lambdaArray: Array[Double]): MatrixFactorizationModel = {
+    val evaluations =
+      for (rank <- rankArray; numIterations <- numIterationsArray; lambda <- lambdaArray) yield {
+        val (rmse, time) = trainModel(trainData, validationData, rank, numIterations, lambda)
+        (rank, numIterations, lambda, rmse)
+      }
+    val eval = evaluations.sortBy(_._4)
+    val bestEval = eval(0)
+    println("BEST PARAMETER: RANK-" + bestEval._1 + ", ITERATIONS- " + bestEval._2 + ", LAMBDA-" + bestEval._3 + ", RMSE-" + bestEval._4)
+    ALS.train(trainData, bestEval._1, bestEval._2, bestEval._3)
+  }
 
 }
